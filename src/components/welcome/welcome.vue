3fd5cf6a3f3c9496106e497935e6f1dee8aa3d0c
unknown <c_yulei-017@SU.intra.cpic.com.cn>
2017-11-29 17:00:45 +0800

    20171129

--- src/components/welcome/welcome.vue
+++ src/components/welcome/welcome.vue
@@ -5,9 +5,9 @@
       <div class="zhuan"></div>
       <div class="jia1"></div>
       <div class="jia2"></div>
-      <!-- <img class="online-xuanke animated" src="images/online-xuanke.png" />
-      <img class="jiajia-ico animated" src="images/jiajia-ico.png" />
-      <img class="jiajia animated" src="images/jiajia.png" /> -->
+      <img class="online-xuanke animated" src="~welcome/online-xuanke.png" />
+      <img class="jiajia-ico animated" src="~welcome/jiajia-ico.png" />
+      <img class="jiajia animated" src="~welcome/jiajia.png" />
 	  </div>
   </div>
 </template>
@@ -16,11 +16,14 @@
   import {getAssociationList} from 'api/association'
   export default {
     created () {
-      this._getAssList()
+      setTimeout(() => {
+        this._getAssList()
+      }, 1500)
     },
     methods: {
       _getAssList () {
         getAssociationList().then((res) => {
+          this.$router.push('associationList')
         })
       }
     }
@@ -30,13 +33,55 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
   @import "~common/stylus/mixin"
-  
   .first-page
     position: absolute
-    // background : green
-    height: 100px
-    width: 100%
-    no-wrap()
-	  bg-image("logo")
-	  // background-size: 100% 100%
+    bg-image("indexbg")
+    background-size: 100% 100%
+    wid-100()
+    hei-100()
+    .wan, .zhuan, .jia1, .jia2
+      height: 1.578125rem
+      position: absolute
+      bg-image("index-title-bg")
+      background-size: cover
+    .wan
+      width: 1.5625rem
+      background-position: 0 0
+      left: 1.015625rem
+      top: 3.734375rem
+      // transform: translateX(1.015625rem) translateY(-3rem) rotateZ(-1200deg)
+    .zhuan
+      width: 1.34375rem
+      background-position: -4.390625rem 0
+      left: 3.21875rem
+      top: 3rem
+    .jia1
+      width: 1.359375rem
+      background-position: -3.03125rem 0
+      left: 5.328125rem
+      top: 3rem
+    .jia2
+      width: 1.34375rem
+      background-position: -1.625rem 0
+      left: 7.390625rem
+      top: 3.734375rem
+    .online-xuanke
+      width: 4.171875rem
+      height: 0.796875rem
+      position: absolute
+      top: 5.90625rem
+      left: 50%
+      margin-left: -2.09375rem
+    .jiajia-ico, .jiajia
+      position: absolute
+    .jiajia-ico
+      width: 1.140625rem
+      height: 0.921875rem
+      left: 0.625rem
+      bottom: 0.34375rem
+    .jiajia
+      width: 4.234375rem
+      height: 0.46875rem
+      left: 1.8125rem
+      bottom: 0.46875rem
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import welcome from '@/components/welcome/welcome'
+import associationList from '@/components/association-list/association-list'
 
 Vue.use(Router)
 
@@ -8,8 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/welcome'
+    },
+    {
+      path: '/welcome',
       name: 'welcome',
       component: welcome
+    },
+    {
+      path: '/associationList',
+      name: 'associationList',
+      component: associationList
     }
   ]
 })
